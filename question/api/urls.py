6a89e6124a73ec442087483f85a42f2a3559f971
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-05-24 02:08:39 +0530

    added gitignore file

--- question/api/urls.py
+++ question/api/urls.py
@@ -6,8 +6,4 @@ urlpatterns = [
     path('<int:id>/', QuizDetailAPIView.as_view()),
     path('<int:id>/questions/', QuestionAPIView.as_view()),
 
-    # path('question/', QuestionAPIView.as_view()),
-    # path('question/<int:id>/', QuestionDetialAPIView.as_view()),
-
-
 ]
